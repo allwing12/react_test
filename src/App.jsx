0620b4d75d31dc7b75fe4feaa43c4163ff9fd352
allwing12 <allwing1232@gmail.com>
2022-08-08 18:15:06 +0900

    react_test

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import TodoEdit from "./components/TodoEdit";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+import TodoListItem from "./components/TodoListItem";
+import TodoTemplate from "./components/TodoTemplate";
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [insertToggle, setInsertToggle] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const nextId = useRef(1);
+
+  const onInsert = (text) => {
+    const todo = {
+      id: nextId.current,
+      text: text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current++;
+  };
+
+  const onInsertToggle = () => {
+    setInsertToggle((prev) => !prev);
+  };
+
+  const onRemove = (id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+
+  const onToggle = async (id) => {
+    const data = await axios({
+      url: `http://localhost:4000/todos/check/${id}`,
+      method: "PATCH",
+    });
+    setTodos(data.data);
+  };
+
+  const onUpdate = (id, text) => {
+    setTodos((todos) =>
+      todos.map((todo) => (todo.id === id ? { ...todo, text } : todo))
+    );
+    onInsertToggle();
+    alert("수정되었습니다.");
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await axios({
+          url: "http://localhost:4000/todos",
+          method: "GET",
+        });
+        console.log(data.data);
+        setTodos(data.data);
+        setIsLoading(false);
+      } catch (e) {
+        setError(e);
+      }
+    };
+    getData();
+  }, []);
+
+  if (error) {
+    return <>에러: {error.message}</>;
+  }
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList
+        todos={todos}
+        onRemove={onRemove}
+        onToggle={onToggle}
+        onInsertToggle={onInsertToggle}
+        setSelectedTodo={setSelectedTodo}
+      />
+      {insertToggle && (
+        <TodoEdit
+          onInsertToggle={onInsertToggle}
+          selectedTodo={selectedTodo}
+          onUpdate={onUpdate}
+        />
+      )}
+    </TodoTemplate>
+  );
+}
+
+export default App;
